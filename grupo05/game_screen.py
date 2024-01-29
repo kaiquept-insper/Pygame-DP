85da9e4dfe1ba73931ad8d03d79d49c7a5658f46
kaiquept-insper <kaiquept@al.insper.edu.br>
2024-01-29 11:08:06 -0300

    formaçao de imagem

--- grupo05/game_screen.py
+++ grupo05/game_screen.py
@@ -1,30 +1,44 @@
 import pygame
 from config import FPS, WIDTH, HEIGHT, BLACK, BLUE, RED
 from assets import *
-dicionario_de_arquivos = carrega_arquivos
-from random import *
+import random
 
 def colisao_entre_retangulos(x1, y1, largura1, altura1, x2, y2, largura2, altura2):
     if (x1 < x2 + largura2 and x1 + largura1 > x2 and y1 < y2 + altura2 and y1 + altura1 > y2):
         return True
     else:
         return False
-    
-def quantidade_imagens(X):
+
+def verifica_colisao(quadrado, list_quadrados):
+    for quadrado2 in list_quadrados:
+        if colisao_entre_retangulos(quadrado["x"], quadrado["y"], 60, 60, quadrado2["x"], quadrado2["y"], 60, 60):
+            return True
+    return False
+        
+
+def quantidade_imagens(X,dicionario_de_arquivos):
     imagem_sorteada = ['cherries', 'flower', 'fruit-tree', 'ladybug', 'tree']
     retorno = []
     
     img_sorteadas = random.sample(imagem_sorteada, X)
 
-    for imagem in imagem_sorteada:
+    for imagem in img_sorteadas:
         qnt_sorteada = random.randint(5,10+1)
 
         for i in range(qnt_sorteada):
-            retorno.append({"image": dicionario_de_arquivos[imagem_sorteada],
+            verificando_quadrado = {"image": dicionario_de_arquivos[imagem],
                         "x": random.randint(60,840),
                         "y": random.randint(60,640),
-                        "tipo": imagem_sorteada})
-    
+                        "tipo": imagem_sorteada}
+            
+            while verifica_colisao(verificando_quadrado,retorno):
+                verificando_quadrado = {"image": dicionario_de_arquivos[imagem],
+                        "x": random.randint(60,840),
+                        "y": random.randint(60,640),
+                        "tipo": imagem_sorteada}
+            
+            retorno.append(verificando_quadrado)
+
     return retorno
 
 def game_screen(window):
@@ -41,6 +55,7 @@ def game_screen(window):
     tempo_da_ultima_mudanca = pygame.time.get_ticks()
 
     # ===== Loop principal =====
+    lista_imagens = quantidade_imagens(1,dicionario_de_arquivos)
     while state != DONE:
         clock.tick(FPS)
 
@@ -51,10 +66,10 @@ def game_screen(window):
                 state = DONE
 
         # ----- Gera saídas
-        window.fill(BLACK)  # Preenche com a cor branca
+        window.fill(BLACK)  # Preenche com a cor preto
         # Lógica para alternar as cores
         agora = pygame.time.get_ticks()
-        if agora - tempo_da_ultima_mudanca > 2000:
+        if agora - tempo_da_ultima_mudanca > 4000:
             tempo_da_ultima_mudanca = agora
             if tela == 'azul':
                 tela = 'vermelha'
@@ -62,10 +77,13 @@ def game_screen(window):
                 tela = 'azul'
 
         if tela == 'azul':
+
             window.fill(BLUE)
         else:
             window.fill(RED)
-        window.blit(dicionario_de_arquivos[CHERRIES], (0,0))
+
+        for objeto in lista_imagens:
+            window.blit(objeto["image"], (objeto["x"],objeto["y"]))
 
         pygame.display.update()  # Mostra o novo frame para o jogador
 
