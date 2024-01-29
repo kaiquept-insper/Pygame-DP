0d29495079d20b612c883f3c1b3beca2186cf9d3
MariaClaraHiranoMotta <mariachm@al.insper.edu.br>
2024-01-29 09:04:50 -0300

    função- colisão de retangulos

--- grupo05/game_screen.py
+++ grupo05/game_screen.py
@@ -2,6 +2,13 @@ import pygame
 from config import FPS, WIDTH, HEIGHT, BLACK, BLUE, RED
 from assets import carrega_arquivos
 
+def colisao_entre_retangulos(x1, y1, largura1, altura1, x2, y2, largura2, altura2):
+    if (x1 < x2 + largura2 and x1 + largura1 > x2 and y1 < y2 + altura2 and y1 + altura1 > y2):
+        return True
+    else:
+        return False
+
+
 def game_screen(window):
     # Variável para o ajuste de velocidade
     clock = pygame.time.Clock()
